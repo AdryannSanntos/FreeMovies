6b09527b23cbc7cea759b8e33e2d016907149dc3
Adryann.Sanntos <nailadryan188@outlook.com.br>
2022-05-18 20:46:52 -0300

    FreeMovies 1.2

---
+++ teste.js
@@ -0,0 +1,14 @@
+const busca = document.getElementById("busca");
+const pesquisar = document.getElementById("pesquisar");
+const pesquisar2 = document.getElementById("pesquisar2");
+
+pesquisar.addEventListener('click',()=>{
+  if(busca.value == "AdminAdryan187781"){
+    window.location.replace("./cadastro.html")
+  }
+})
+pesquisar2.addEventListener('click',()=>{
+  if(busca.value == "AdminAdryan187781"){
+    window.location.replace("./cadastro.html")
+  }
+})
